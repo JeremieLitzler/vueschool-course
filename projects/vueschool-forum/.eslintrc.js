37b7342ae39af4c295c8cae31f7870a8dabddad5
Jérémie LITZLER <jlitzler@serial.ch>
2023-10-18 10:19:02 +0200

    vuecli: init project

---
+++ projects/vueschool-forum/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+};
