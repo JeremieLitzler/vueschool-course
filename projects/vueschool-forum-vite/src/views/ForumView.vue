ba357ddadd7976e8ea608171aea1d8c36a5438fb
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-19 09:34:42 +0200

    complete Styling Vue Components

--- projects/vueschool-forum-vite/src/views/ForumView.vue
+++ projects/vueschool-forum-vite/src/views/ForumView.vue
@@ -1,18 +1,39 @@
 <template>
-  <h1>What's new on the forum?</h1>
-  <section v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <article v-for="postId in thread.posts" :key="postId">
-      <p>
-        <em
-          ><strong>
-            {{ userById(postById(postId).userId).name }}
-          </strong>
-        </em>
-      </p>
-      <p>{{ postById(postId).text }}</p>
-    </article>
-  </section>
+  <div class="container">
+    <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+      <h1>{{ thread.title }}</h1>
+
+      <div class="post-list">
+        <div class="post" v-for="postId in thread.posts" :key="postId">
+          <div class="user-info">
+            <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
+
+            <a href="#">
+              <img
+                class="avatar-large"
+                :src="userById(postById(postId).userId).avatar"
+                :alt="userById(postById(postId).userId).name"
+              />
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>
+              <p>
+                {{ postById(postId).text }}
+              </p>
+            </div>
+          </div>
+
+          <div class="post-date text-faded">
+            {{ postById(postId).publishedAt }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
--- projects/vueschool-forum/src/components/PageHome.vue
+++ projects/vueschool-forum/src/components/PageHome.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>Home page</div>
-  <section v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <article v-for="postId in thread.posts" :key="postId">
-      <p>
-        <em
-          ><strong>
-            {{ userById(postById(postId).userId).name }}
-          </strong>
-        </em>
-      </p>
-      <p>{{ postById(postId).text }}</p>
-    </article>
-  </section>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+
+    <div class="post-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{
+            userById(postById(postId).userId).name
+          }}</a>
+
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="userById(postById(postId).userId).avatar"
+              :alt="userById(postById(postId).userId).name"
+            />
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>
+              {{ postById(postId).text }}
+            </p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{ postById(postId).publishedAt }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,5 +56,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
