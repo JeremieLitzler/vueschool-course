6e22a82165ae7d2163d4f617dd6be176b7799d6c
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-19 08:41:58 +0200

    complete Displaying a List of Elements in Vue 3

--- projects/vueschool-forum/src/components/PageHome.vue
+++ projects/vueschool-forum/src/components/PageHome.vue
@@ -1,11 +1,37 @@
 <template>
   <div>Home page</div>
+  <section v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <article v-for="postId in thread.posts" :key="postId">
+      <p>
+        <em
+          ><strong>
+            {{ userById(postById(postId).userId).name }}
+          </strong>
+        </em>
+      </p>
+      <p>{{ postById(postId).text }}</p>
+    </article>
+  </section>
 </template>
 
 <script>
+import sourceData from "@/data.json";
 export default {
-  setup() {
-    return {};
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((post) => post.id === postId);
+    },
+    userById(userId) {
+      return this.users.find((user) => user.id === userId);
+    },
   },
 };
 </script>
