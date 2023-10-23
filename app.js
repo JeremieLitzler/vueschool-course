067c4ee6ebdd8b7e8d296fa1da9af1803ee6e5cf
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-23 17:11:56 +0200

    complete Display Error Messages After User Input

---
+++ app.js
@@ -0,0 +1,56 @@
+console.log('Loading Vue...');
+
+Vue.use(vuelidate.default);
+new Vue({
+  el: '#app',
+  data() {
+    return { form: { name: null, age: null } };
+  },
+  validations: {
+    form: {
+      name: {
+        required: validators.required,
+      },
+      age: {
+        required: validators.required,
+        integer: validators.integer,
+        // min: validators.minValue(12),
+        // max: validators.maxValue(120),
+        // IMPORTANT: "between" include the value specified so it behave as "<=" or ">=" operators
+        between: validators.between(12, 120),
+      },
+    },
+  },
+  computed: {
+    nameIsValid() {
+      return !!this.form.name;
+    },
+    ageIsValid() {
+      console.log(`Age is number >`, typeof this.form.age, this.form.age);
+      return (
+        typeof this.form.age === 'number' &&
+        this.form.age > 12 &&
+        this.form.age < 120
+      );
+    },
+    formIsValid() {
+      return this.nameIsValid && this.ageIsValid;
+    },
+  },
+  methods: {
+    submitFormVanilla() {
+      if (this.formIsValid) {
+        console.log(this.form);
+      } else {
+        console.error('❌ The form is invalid !');
+      }
+    },
+    submitFormWithVuelidate() {
+      if (!this.$v.form.$invalid) {
+        console.log(this.form);
+      } else {
+        console.error('❌ The form is invalid !');
+      }
+    },
+  },
+});
