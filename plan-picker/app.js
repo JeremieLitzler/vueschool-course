73780c9fb6c27aba6edd357b0f9324d2bcc9bed6
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-24 15:00:38 +0200

    complete Reusable Components with Props & Nested Components

---
+++ plan-picker/app.js
@@ -0,0 +1,20 @@
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return { plans: ['The Single', 'The Curious', 'The Addict'] };
+  },
+});
+
+Vue.component('plan-item', {
+  template: '#plan-template',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+new Vue({
+  el: '#app',
+});
