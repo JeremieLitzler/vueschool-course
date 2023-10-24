e527abf0a61661bd52fb357746a118fa21bcdbd2
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-24 17:11:41 +0200

    complete Component Naming Best Practices

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,12 +1,46 @@
-Vue.component('todo-item', {
-  template: '#todo-item-template',
+let PlanPickerItemComponent = {
+  template: '#plan-picker-item-template',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    selectedPlan: {
+      type: String,
+    },
+  },
+  computed: {
+    isSelected() {
+      return this.selectedPlan === this.name;
+    },
+  },
+  methods: {
+    select() {
+      this.$emit('select-plan', this.name);
+    },
+  },
+};
+let PlanPickerComponent = {
+  template: '#plan-picker-template',
+  components: {
+    'plan-picker-item': PlanPickerItemComponent,
+  },
   data() {
     return {
-      completed: false,
+      plans: ['The Single', 'The Curious', 'The Addict'],
+      selectedPlan: null,
     };
   },
-});
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan;
+    },
+  },
+};
 
 new Vue({
   el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent,
+  },
 });
