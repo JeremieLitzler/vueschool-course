52a0c5c06ff8371b47e355f1a0e98c75adaba2dc
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-10-31 08:35:39 +0100

    complete Create, Import and Use Single File Components

---
+++ awesome-single-file/src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.username {
+  font-size: 10rem;
+  color: gold;
+}
+</style>
