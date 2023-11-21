551f5626d93ab00066db72b6dcb6657046842606
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-11-21 10:54:08 +0100

    add projet without source repo .git folder

---
+++ project-random-users/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppUserList>
+    <template #title>
+      <h1>An awesome user list</h1>
+    </template>
+    <template #loading>
+      <AppSpinner />
+    </template>
+  </AppUserList>
+</template>
+
+<script>
+import AppUserList from "@/components/AppUserList";
+import AppSpinner from "@/components/AppSpinner";
+
+export default {
+  components: {
+    AppUserList,
+    AppSpinner,
+  },
+};
+</script>
---
+++ project-random-users/src/components/AppLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="layout">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer">made with slots</slot>
+    </footer>
+  </div>
+</template>
+
+
+<style scoped>
+.layout {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.layout header,
+.layout footer {
+  background: #41b883;
+  color: white;
+  padding: 1rem;
+  flex: 0 0 auto;
+}
+
+.layout main {
+  flex: 1 1 auto;
+}
+</style>
