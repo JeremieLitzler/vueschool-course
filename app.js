704590c73d6f0930c6e455e2c310ca6461ca4e5e
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-08 14:21:14 +0100

    HTML Attribute Binding in Vue 3 + Dynamic CSS classes with Vue 3

--- app.js
+++ app.js
@@ -6,21 +6,31 @@ Vue.createApp({
       newItem: '',
       newItemHighPriority: false,
       items: [
-        // {id: 1, label: "Food"}, {id:2, label:"Tools"}, {id:3, label: "Paper"}
+        { id: 1, label: 'Food', purchased: true, highPriority: true },
+        { id: 2, label: 'Tools', purchased: false, highPriority: true },
+        { id: 3, label: 'Paper', purchased: true, highPriority: false },
       ],
     };
   },
   methods: {
     saveitem() {
-      this.items.push({ id: this.items.length + 1, label: this.newItem });
+      this.items.push({
+        id: this.items.length + 1,
+        label: this.newItem,
+        highPriority: this.newItemHighPriority,
+      });
       this.clearInputs();
     },
     doEdit(editing) {
       this.editing = editing;
       this.clearInputs();
     },
+    togglePurchase(item) {
+      item.purchased = !item.purchased;
+    },
     clearInputs() {
       this.newItem = '';
+      this.newItemHighPriority = false;
     },
   },
 }).mount('#shopping-list');
