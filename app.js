822fd86a010f42f082e49a2f91b5310b9a98b620
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-08 13:44:39 +0100

    complete Conditional Rendering in Vue 3

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-console.log('Loading Vue...');
-new Vue({
-  el: '#app',
+Vue.createApp({
   data() {
-    return { };
-  },
-  computed: {
+    return {
+      header: 'Shopping list app',
+      editing: false,
+      newItem: '',
+      newItemHighPriority: false,
+      items: [
+        // {id: 1, label: "Food"}, {id:2, label:"Tools"}, {id:3, label: "Paper"}
+      ],
+    };
   },
   methods: {
+    saveitem() {
+      this.items.push({ id: this.items.length + 1, label: this.newItem });
+      this.clearInputs();
+    },
+    doEdit(editing) {
+      this.editing = editing;
+      this.clearInputs();
+    },
+    clearInputs() {
+      this.newItem = '';
+    },
   },
-});
+}).mount('#shopping-list');
