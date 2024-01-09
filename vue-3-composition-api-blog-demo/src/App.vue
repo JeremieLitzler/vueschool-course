8d41429cd2eb32298f5bc83a2eb055971367216c
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-09 12:01:20 +0100

    init blog project

---
+++ vue-3-composition-api-blog-demo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="p-5 bg-green-400 text-white mb-10">
+    <router-link :to="{ name: 'Home' }">Snazzy Fake Blog</router-link>
+  </nav>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
+<style lang="css">
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+}
+</style>
