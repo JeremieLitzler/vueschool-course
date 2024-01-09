6086efabb99f3931290b03bca54252ad585df510
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-09 09:42:22 +0100

    complete The Setup Method's Parameters: props and context

--- vue3-composition-api-demo/src/App.vue
+++ vue3-composition-api-demo/src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import YummyMeal from "./components/YummyMeal.vue";
+    export default {
+      components:{
+        YummyMeal
+      },
+      setup() {
+        const name = "The Snazzy Burger";
+        const placeOrder = () => {
+          alert("You've ordered a great meal!")
+        };
+        const addItemToCart = (item) => {
+          alert(`One ${item} was added to the cart`)
+        }
+        return { name, placeOrder, addItemToCart };
+      }
+    }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ name }}</h1>
+  <button @click="placeOrder">Order</button>
+  <YummyMeal name="Hamburger" :price="5" @addToCart="addItemToCart" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 </style>
---
+++ vue3-composition-api-demo/src/components/YummyMeal.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    props: {
+      name: String,
+      price: Number
+    },
+    setup(props, { emit}){
+      const addToCart = () => emit("addToCart", props.name);
+      return {
+        addToCart
+      }
+    }
+  }
+</script>
+
+<template>
+  <h2>{{ name }} @ {{price }}</h2>
+  <button @click="addToCart">Add to cart</button>
+</template>
+
+<style scoped></style>
