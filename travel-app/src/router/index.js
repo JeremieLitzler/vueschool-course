c28a6c645b23d1fb4873c8a4907698d7aa5cbd4b
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-15 16:24:18 +0100

    complete Scroll Behavior

--- travel-app/src/router/index.js
+++ travel-app/src/router/index.js
@@ -55,6 +55,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return (
+      savedPosition ||
+      new Promise((resolve) => {
+        setTimeout(() => resolve({ top: 0, behavior: 'smooth' }), 300);
+      })
+    );
+  },
 });
 
 export default router;
