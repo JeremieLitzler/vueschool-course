fa99ed4f05f50e37e21a605f9b1cf114da1e00c7
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-18 09:35:25 +0100

    complete Detecting Navigation Failures

--- travel-app/src/views/Home.vue
+++ travel-app/src/views/Home.vue
@@ -18,16 +18,28 @@
 
 <script setup>
   import { reactive } from "vue";
-  import { useRouter } from "vue-router";
   import sourceData from "@/data.json";
   console.log(sourceData);
   const destinations = reactive(sourceData.destinations);
 
+  import { NavigationFailureType, isNavigationFailure, useRouter } from "vue-router";
   const router = useRouter();
   const triggerRouterError = async () => {
     //let navigate to the home page while on the home page...
-    const result = await router.push('/');
-    console.log(result);
+    const navigationResult = await router.push('/');
+    console.log(navigationResult);
+    if (isNavigationFailure(navigationResult, NavigationFailureType.duplicated)) {
+      //the route is the current and cannot be navigated to
+    }
+    else if (isNavigationFailure(navigationResult, NavigationFailureType.aborted)) {
+      //false is returned in a navigation guard
+    }
+    else if (isNavigationFailure(navigationResult, NavigationFailureType.cancelled)) {
+      //a new navigation took place before the current could finish.
+    }
+    else {
+      //all was fine.
+    }
   }
 </script>
 
