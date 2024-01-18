5ae2922f4e5bd24d29fd698ac25dfb71e2f88891
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-18 11:06:23 +0100

    complete Init vuex project

---
+++ shopping-cart-app/src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :id="product.id">
+        {{ product.title }} - {{ product.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import shop from '@/api/shop';
+
+  const products = ref([]);
+
+  shop.getProducts(data => {
+    products.value = data;
+  });
+</script>
+
+<style lang="scss" scoped></style>
