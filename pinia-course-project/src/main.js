7fff0e52bebb02f52b309e91e694c22fa8987fef
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-19 15:56:13 +0100

    completed Install and Setup Up Pinia

--- pinia-course-project/src/main.js
+++ pinia-course-project/src/main.js
@@ -1,19 +1,21 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
 // Icons and Styles
-import FontAwesomePlugin from "./plugins/FontAwesome";
-import "./assets/main.pcss";
+import FontAwesomePlugin from './plugins/FontAwesome';
+import './assets/main.pcss';
 
 // App Wide Components
-import AppButton from "./components/AppButton.vue";
-import AppCountInput from "./components/AppCountInput.vue";
-import AppModalOverlay from "./components/AppModalOverlay.vue";
+import AppButton from './components/AppButton.vue';
+import AppCountInput from './components/AppCountInput.vue';
+import AppModalOverlay from './components/AppModalOverlay.vue';
 
 // Init App
 createApp(App)
+  .use(createPinia())
   .use(FontAwesomePlugin)
-  .component("AppButton", AppButton)
-  .component("AppCountInput", AppCountInput)
-  .component("AppModalOverlay", AppModalOverlay)
-  .mount("#app");
+  .component('AppButton', AppButton)
+  .component('AppCountInput', AppCountInput)
+  .component('AppModalOverlay', AppModalOverlay)
+  .mount('#app');
