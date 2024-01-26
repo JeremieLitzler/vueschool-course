0329caba0f5838f6271b457dbe1386abd8d8dac2
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 14:26:20 +0100

    complete using lifecycle hooks

--- vue3-custom-directive-example/src/App.vue
+++ vue3-custom-directive-example/src/App.vue
@@ -1,30 +1,38 @@
 <script setup>
   import { ref } from 'vue';
 
+  const show = ref(true);
   const colorsPickerArr = ref(['green', 'red', 'gold']);
-  const vColor = (element, binding) => {
-    let i = 0;
-    const speedsMs = {
-      slow: 2000,
-      normal: 1000,
-      fast: 500,
-    };
+  const vColor = {
+    mounted(element, binding) {
+      let i = 0;
+      const speedsMs = {
+        slow: 2000,
+        normal: 1000,
+        fast: 500,
+      };
 
-    const speedName = binding.arg || 'normal';
-    const speed = speedsMs[speedName];
-    const colors =  binding.value;
+      const speedName = binding.arg || 'normal';
+      const speed = speedsMs[speedName];
+      const colors =  binding.value;
 
-    if(binding.modifiers.underline) {
-      element.style.textDecoration = "underline";
+      if(binding.modifiers.underline) {
+        element.style.textDecoration = "underline";
+      }
+      if(binding.modifiers.italic) {
+        element.style.fontStyle = "italic";
+      }
+      //The double underscores are there to avoid name collisions.
+      element.__ColorInterval__ = setInterval(() => {
+        console.log("🖌️ Coloring");
+        element.style.color = colors[i];
+        i++;
+        if (i === colors.length) i = 0;
+      }, speed);
+    },
+    unmounted(element) {
+      clearInterval(element.__ColorInterval__);
     }
-    if(binding.modifiers.italic) {
-      element.style.fontStyle = "italic";
-    }
-    setInterval(() => {
-      element.style.color = colors[i];
-      i++;
-      if (i === colors.length) i = 0;
-    }, speed);
   }
 
   setTimeout(() => {
@@ -33,5 +41,6 @@
 </script>
 
 <template>
-  <h1 v-color:slow.italic="colorsPickerArr">My title</h1>
+  <h1 v-if="show" v-color:slow.italic="colorsPickerArr">My title</h1>
+  <button @click="show = !show">Toogle</button>
 </template>
