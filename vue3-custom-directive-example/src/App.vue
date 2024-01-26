25871ac486de8029208b523e1c0c1637232acc70
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 12:46:47 +0100

    complete Create a Custom Vue.js 3 Directive

--- vue3-custom-directive-example/src/App.vue
+++ vue3-custom-directive-example/src/App.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+  const vColor = (element) => element.style.color = "gold"
+</script>
 
 <template>
-  <div></div>
+  <h1 v-color>My title</h1>
 </template>
 
 <style scoped></style>
