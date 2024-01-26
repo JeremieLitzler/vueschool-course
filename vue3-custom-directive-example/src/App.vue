5212c7f1f7d02aaeae4d1d211916df85e051bb94
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 12:57:13 +0100

    complete The Custom Directive's Value

--- vue3-custom-directive-example/src/App.vue
+++ vue3-custom-directive-example/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-  const vColor = (element) => element.style.color = "gold"
+  const vColor = (element, binding) => {
+    let i = 0;
+    const colors =  binding.value;
+    setInterval(() => {
+        element.style.color = colors[i];
+        i++;
+        if (i === colors.length) i = 0;
+    }, 500);
+    }
 </script>
 
 <template>
-  <h1 v-color>My title</h1>
+  <h1 v-color="['green', 'red', 'gold']">My title</h1>
 </template>
-
-<style scoped></style>
