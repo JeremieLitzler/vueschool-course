6ee33adec4d6c165139faeadf10721e39df39402
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 07:28:06 +0100

    complete Typing Computed Props

--- journal-app/src/components/EntryEditor.vue
+++ journal-app/src/components/EntryEditor.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import type { Ref } from "vue";
 
 import EmojiField from "@/components/EmojiField.vue";
@@ -7,17 +7,19 @@ import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import type Emoji from "@/types/Emoji";
 
 const entryMessage = ref("");
+const entryMessageLength = computed(() => entryMessage.value.length);
 const emoji: Ref<Emoji | null> = ref(null);
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
       v-model="entryMessage"
+      maxlength="280"
       placeholder="New Journal Entry for danielkelly_io"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ entryMessageLength }} / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
