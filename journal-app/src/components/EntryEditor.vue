7151c6ba53a01f7fb35b9f50d31ed7fbc7d2798c
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 07:48:58 +0100

    complete Declaring Component Events

--- journal-app/src/components/EntryEditor.vue
+++ journal-app/src/components/EntryEditor.vue
@@ -6,13 +6,18 @@ import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import type Emoji from "@/types/Emoji";
 
+defineEmits(["@addEntry"]);
+
 const MAX_CHARS = 300;
 const entryMessage = ref("");
 const entryMessageLength = computed(() => entryMessage.value.length);
 const emoji: Ref<Emoji | null> = ref(null);
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
+  <form
+    class="entry-form"
+    @submit.prevent="$emit('@addEntry', { entryMessage, emoji })"
+  >
     <textarea
       v-model="entryMessage"
       :maxlength="MAX_CHARS"
