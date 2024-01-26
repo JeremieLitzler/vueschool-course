7cc5bcf06c038e793d6f9b48d1330b8525444d46
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-26 13:28:29 +0100

    complete Vue.js Custom Directive Argument

--- vue3-custom-directive-example/src/App.vue
+++ vue3-custom-directive-example/src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
+  import { ref } from 'vue';
+
+  const colorsPickerArr = ref(['green', 'red', 'gold']);
   const vColor = (element, binding) => {
     let i = 0;
+    const speedsMs = {
+      slow: 2000,
+      normal: 1000,
+      fast: 500,
+    };
+
+    const speedName = binding.arg || 'normal';
+    const speed = speedsMs[speedName];
     const colors =  binding.value;
     setInterval(() => {
-        element.style.color = colors[i];
-        i++;
-        if (i === colors.length) i = 0;
-    }, 500);
-    }
+      element.style.color = colors[i];
+      i++;
+      if (i === colors.length) i = 0;
+    }, speed);
+  }
+
+  setTimeout(() => {
+    colorsPickerArr.value.push("orange")
+  }, 5000);
 </script>
 
 <template>
-  <h1 v-color="['green', 'red', 'gold']">My title</h1>
+  <h1 v-color:slow="colorsPickerArr">My title</h1>
 </template>
