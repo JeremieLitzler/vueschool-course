c86c488c9a93d2c244923c81cc80f48898ec2b54
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-29 11:42:13 +0100

    shortcusts, extensions and co

---
+++ src/components/MyAwesomeNewComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <section id="section-class">
+      <p id="id-1">Item 1</p>
+      <article id="id-2">Item 2</article>
+      <article id="id-3">Item 3</article>
+      <article id="id-4">Item 4</article>
+    </section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+#section-class {
+  background-clip: border-box;
+}
+</style>
