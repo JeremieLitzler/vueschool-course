c3243649bc44e2ff141086f3f24ee41ebb41f504
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-01-29 13:39:07 +0100

    make component functionnal

--- src/components/user-card.vue
+++ src/components/user-card.vue
@@ -5,25 +5,15 @@
       <div class="card">
         <div class="additional">
           <figure class="avatar">
-            <img :src="user.avatar" :alt="user.name" />
+            <img :src="props.user.avatar" :alt="props.user.name" />
           </figure>
         </div>
         <div class="more-info">
-          <h1>{{ user.name }}</h1>
-          <h3>{{ user.role }}</h3>
-          <p>{{ user.bio }}</p>
+          <h1>{{ props.user.name }}</h1>
+          <h3>{{ props.user.role }}</h3>
+          <p>{{ props.user.bio }}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "Card",
-
-  props: {
-    user: Object,
-  },
-};
-</script>
