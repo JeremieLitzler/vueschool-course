bf9b971671c14bb8da85ce2c4a74200d6304591a
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-01 16:11:44 +0100

    complete Customize the Form with Props

--- app.vue
+++ app.vue
@@ -10,15 +10,26 @@ async function submit(data: Object): void {
 }
 </script>
 <template>
-  <FormKit type="form" :value="formData" @submit="submit">
-    <h1>Login</h1>
-    <FormKit type="text" label="Username" name="username" />
-    <FormKit
-      type="password"
-      label="Password"
-      name="password"
-      validation="required"
-    />
-    <FormKit type="checkbox" label="Remember me?" name="rememberMe" />
+  <FormKit
+    type="form"
+    :value="formData"
+    @submit="submit"
+    submit-label="Login"
+    :actions="false"
+  >
+    <template #default="{ state }">
+      <h1>Login</h1>
+      <FormKit type="text" label="Username" name="username" />
+      <FormKit
+        type="password"
+        label="Password"
+        name="password"
+        validation="required"
+      />
+      <FormKit type="checkbox" label="Remember me?" name="rememberMe" />
+      <button :disabled="state.loading">
+        {{ state.loading ? "Logging-in you..." : "Login" }}
+      </button>
+    </template>
   </FormKit>
 </template>
