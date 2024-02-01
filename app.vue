eb152816d04be8d11e17dcee1257fe996bca7ccb
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-01 19:03:34 +0100

    complete Create Custom Validation Messages

--- app.vue
+++ app.vue
@@ -12,12 +12,20 @@ async function submit(data: Object): void {
 <template>
   <FormKit type="form" :value="formData" @submit="submit" submit-label="Login">
     <h1>Login</h1>
-    <FormKit type="text" label="Username" name="username" />
+    <FormKit
+      validation="(500)username_is_unique"
+      :validation-messages="{ username_is_unique({args, name, node}) : string {
+        return `${node.value} is already taken!`;
+      }}"
+      type="text"
+      label="Username"
+      name="username"
+    />
     <FormKit
       type="password"
       label="Password"
       name="password"
-      validation="required"
+      validation="required|alpha|length:10-30"
     />
     <FormKit type="checkbox" label="Remember me?" name="rememberMe" />
   </FormKit>
--- formkit.config.ts
+++ formkit.config.ts
@@ -2,6 +2,22 @@ import type { DefaultConfigOptions } from "@formkit/vue";
 
 const config: DefaultConfigOptions = {
   theme: "genesis",
+  messages: {
+    en: {
+      validation: {
+        username_is_unique({ args, name, node }) {
+          return `${node.value} is already taken`;
+        },
+      },
+    },
+    fr: {
+      validation: {
+        username_is_unique({ args, name, node }) {
+          return `${node.value} est déjà pris`;
+        },
+      },
+    },
+  },
   rules: {
     username_is_unique(node) {
       const usernames = ["jpschroeder", "luanguyen", "danielkelly_io"];
