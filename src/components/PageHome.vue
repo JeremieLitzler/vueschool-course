6ef4595a8d0147c56a7739070a5eb60b2933783b
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-13 15:26:59 +0100

    complete Create ThreadList.vue and Clean up the Homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,3 +1,15 @@
-<template><div></div></template>
+<template>
+  <h1>Welcome to the forum</h1>
+  <ThreadList />
+</template>
 
-<script></script>
+<script>
+import ThreadList from "./ThreadList.vue";
+export default {
+  components: {
+    ThreadList,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ThreadList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="col-full">
+    <div class="thread-list">
+      <h2 class="list-title">Threads</h2>
+
+      <div class="thread" v-for="thread in threads" :key="thread.id">
+        <div>
+          <p>
+            <a href="#">{{ thread.title }}</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="profile.html">{{ userById(thread.userId).name }}</a
+            >, {{ thread.publishedAt }}.
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+            {{ thread.posts.length }} repl{{
+              thread.posts.length > 1 ? "ies" : "y"
+            }}
+          </p>
+
+          <img
+            class="avatar-medium"
+            :src="userById(thread.userId).avatar"
+            :alt="`Avatar of ${userById(thread.userId).name}`"
+          />
+
+          <div>
+            <p class="text-xsmall">
+              <a href="profile.html">{{ userById(thread.userId).name }}</a>
+            </p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="pagination">
+      <button class="btn-circle" disabled>
+        <i class="fa fa-angle-left"></i>
+      </button>
+      1 of 3
+      <button class="btn-circle"><i class="fa fa-angle-right"></i></button>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      const match = this.posts.find((post) => post.id === postId);
+      //   console.log("postById", match);
+      return match;
+    },
+    userById(userId) {
+      const match = this.users.find((user) => user.id === userId);
+      //   console.log("userById", match);
+      return match;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
