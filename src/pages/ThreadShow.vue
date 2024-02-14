26954e6bc53a43e203c2a1f924f969294bdca570
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-14 16:40:49 +0100

    complete Creating a Post Editor

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -2,6 +2,21 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <post-list :posts="threadPosts" />
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea
+            v-model="newPostText"
+            cols="30"
+            rows="10"
+            class="form-input"
+          />
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -23,6 +38,7 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
+      newPostText: "",
     };
   },
   computed: {
@@ -34,6 +50,21 @@ export default {
       const matches = this.posts.filter((post) => post.threadId === this.id);
       console.log("threadPosts", matches);
       return matches;
+    },
+  },
+  methods: {
+    addPost() {
+      const postId = "gggg" + Math.random();
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: "38St7Q8Zi2N1SPa5ahzssq9kbyp1",
+      };
+      this.posts.push(post);
+      this.thread.posts.push(postId);
+      this.newPostText = "";
     },
   },
 };
