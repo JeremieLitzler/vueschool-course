60c99e0fbe4a92a71ac6eecf4bd45ac10befdb67
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-19 14:46:40 +0100

    complete Fetching the Authenticated User From Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <the-nav-bar />
+  <the-header />
   <div class="container">
     <router-view />
   </div>
 </template>
 
 <script>
-import TheNavBar from "@/components/TheNavBar.vue";
+import TheHeader from "@/components/TheHeader.vue";
 export default {
   name: "App",
-  components: { TheNavBar },
+  components: { TheHeader },
 };
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header" id="header">
+    <router-link :to="{ name: 'TheHome' }">
+      <img src="../assets/img/svg/vueschool-logo.svg" />
+    </router-link>
+
+    <div class="btn-hamburger">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+
+    <the-nav-bar />
+  </header>
+</template>
+
+<script>
+import TheNavBar from "./TheNavBar.vue";
+export default {
+  components: {
+    TheNavBar,
+  },
+};
+</script>
