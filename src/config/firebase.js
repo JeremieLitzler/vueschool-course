9f17cab3efdab4f180c3dfdb4747d223c35533c6
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-02-29 16:57:42 +0100

    complete Using .env for Secure Secrets

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: process.env.FIRESTORE_API_KEY,
-  authDomain: "vueschool-forum-35ad5.firebaseapp.com",
-  projectId: "vueschool-forum-35ad5",
-  storageBucket: "vueschool-forum-35ad5.appspot.com",
-  messagingSenderId: "573844941700",
-  appId: "1:573844941700:web:ef74680e59414b9e754bd1",
+  apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIRESTORE_APP_ID,
 };
 
 export { firebaseConfig };
