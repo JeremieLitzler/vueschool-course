dfca5c15c85caf588ffd15ca0f5da6bb312b8d41
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-01 08:24:37 +0100

    complete Using .env for Secure Secrets

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
   //https://github.com/vitejs/vite/issues/1973
-  apiKey: import.meta.env.VUE_APP_FIRESTORE_API_KEY,
-  authDomain: import.meta.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
-  projectId: import.meta.env.VUE_APP_FIRESTORE_PROJECT_ID,
-  storageBucket: import.meta.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VUE_APP_FIRESTORE_APP_ID,
+  apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
+  authDomain: import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRESTORE_APP_ID,
 };
 
 export { firebaseConfig };
