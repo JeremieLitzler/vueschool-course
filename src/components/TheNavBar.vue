023fd521406015f3ae8d558553c50719ef1f8856
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-06 12:12:31 +0100

    complete Building a Mobile Navbar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,12 +1,26 @@
 <template>
+  <div
+    class="btn-hamburger"
+    @click="toggleMobileMenu"
+    v-click-outside="closeMobileMenu"
+  >
+    <!-- use .btn-humburger-active to open the menu -->
+    <div class="top bar"></div>
+    <div class="middle bar"></div>
+    <div class="bottom bar"></div>
+  </div>
   <!-- use .navbar-open to open nav -->
-  <nav class="navbar">
+  <nav class="navbar" :class="{ 'navbar-open': mobileMenuOpened }">
     <span v-if="!useCommonStore().appIsReady" class="navbar-user navbar-loading"
       ><app-spinner background-color="#ffffff"
     /></span>
     <ul v-else>
       <li v-if="signedIn" class="navbar-user">
-        <a @click.prevent="toggleMenu" v-click-outside="closeDropdown" href="#">
+        <a
+          @click.prevent="userMenuOpened = !userMenuOpened"
+          v-click-outside="closeDropdown"
+          href="#"
+        >
           <img
             class="avatar-small"
             :src="user?.avatar"
@@ -24,7 +38,7 @@
 
         <!-- dropdown menu -->
         <!-- add class "active-drop" to show the dropdown -->
-        <div id="user-dropdown" :class="{ 'active-drop': menuOpened }">
+        <div id="user-dropdown" :class="{ 'active-drop': userMenuOpened }">
           <div class="triangle-drop"></div>
           <ul class="dropdown-menu">
             <li class="dropdown-menu-item">
@@ -46,35 +60,18 @@
       <li v-if="!signedIn" class="navbar-item">
         <router-link :to="{ name: RouteName.UserLogin }">Login</router-link>
       </li>
-    </ul>
-
-    <ul>
-      <li class="navbar-item">
-        <router-link :to="{ name: RouteName.TheHome }">Home</router-link>
+      <li v-if="signedIn" class="navbar-mobile-item">
+        <router-link :to="{ name: 'AccountShow' }">View profile</router-link>
       </li>
-
-      <!-- <li class="navbar-item">
-            <a href="category.html">Category</a>
-          </li>
-          <li class="navbar-item">
-            <a href="forum.html">Forum</a>
-          </li>
-          <li class="navbar-item">
-            <a href="thread.html">Thread</a>
-          </li>
-          -->
-      <!-- Show these option only on mobile-->
-      <!-- <li class="navbar-item mobile-only">
-            <a href="profile.html">My Profile</a>
-          </li>
-          <li class="navbar-item mobile-only">
-            <a href="#">Logout</a>
-          </li> -->
+      <li v-if="signedIn" class="navbar-mobile-item">
+        <a @click.prevent="logout">Log out</a>
+      </li>
     </ul>
   </nav>
 </template>
 <script setup lang="ts">
 import { ref, computed } from 'vue';
+
 import { RouteName } from '@/enums/RouteName';
 import { useUserStore } from '@/stores/UserStore';
 import useAppendRouteHelper from '@/helpers/appendRouteHelper';
@@ -82,18 +79,26 @@ import { useCommonStore } from '@/stores/CommonStore';
 
 const { toSignOut } = useAppendRouteHelper();
 
-const menuOpened = ref(false);
+const userMenuOpened = ref(false);
+const mobileMenuOpened = ref(false);
 const user = computed(() => useUserStore().getAuthUser());
 
 const signedIn = computed(
   () => user.value.id != '' || useUserStore().authId != ''
 );
+const toggleMobileMenu = () => {
+  console.log('clicked toggleMobileMenu', mobileMenuOpened.value);
 
-const toggleMenu = () => (menuOpened.value = !menuOpened.value);
+  mobileMenuOpened.value = !mobileMenuOpened.value;
+};
 const closeDropdown = () => {
-  menuOpened.value = false;
+  userMenuOpened.value = false;
+};
+const closeMobileMenu = () => {
+  mobileMenuOpened.value = false;
 };
 const logout = async () => {
+  mobileMenuOpened.value = false;
   toSignOut();
 };
 </script>
