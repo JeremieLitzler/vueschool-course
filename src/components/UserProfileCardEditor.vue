b836302c93e8c0e2a9a47ef202488e435bfc47c2
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-12 11:10:36 +0100

    complete Improving the Avatar Editing User Experience

--- src/components/UserProfileCardEditor.vue
+++ src/components/UserProfileCardEditor.vue
@@ -2,12 +2,23 @@
   <div class="profile-card">
     <form @submit.prevent="saveProfile">
       <p class="text-center">
-        <label for="avatar">
-          <img
-            :src="avatarPreview"
-            :alt="`${user?.name} profile picture`"
-            class="avatar-xlarge img-update"
-          />
+        <label
+          aria-labelledby="avatar-change"
+          for="avatar"
+          class="avatar-label"
+        >
+          <div class="wrapper avatar-xlarge">
+            <div id="avatar-change" class="content">
+              <span>Modify 📷</span>
+            </div>
+            <div class="underlay">
+              <img
+                :src="avatarPreview"
+                :alt="`${user?.name} profile picture`"
+                class="avatar-xlarge img-update"
+              />
+            </div>
+          </div>
         </label>
         <input
           v-show="false"
@@ -84,7 +95,7 @@
       </div>
 
       <div class="btn-group space-between">
-        <button class="btn-ghost">Cancel</button>
+        <button @click.prevent="cancel" class="btn-ghost">Cancel</button>
         <button type="submit" class="btn-blue">Save</button>
       </div>
     </form>
@@ -129,6 +140,9 @@ export default {
       this.$store.dispatch("users/updateUser", { ...this.editedUser });
       this.$router.push({ name: RouteName.AccountShow });
     },
+    cancel() {
+      this.$router.push({ name: RouteName.AccountShow });
+    },
     handleImageUpload(event) {
       this.editedUser.avatar = event.target.files[0];
       const reader = new FileReader();
@@ -140,3 +154,46 @@ export default {
   },
 };
 </script>
+<style>
+.wrapper {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  margin: 1em auto;
+  background: lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  position: relative;
+  font-weight: bold;
+}
+
+.content {
+  position: relative;
+  z-index: 1;
+  display: flex;
+  align-items: end;
+}
+
+.content span {
+  font-size: 1.4em;
+  color: white;
+  margin-bottom: 1.2em;
+  background-color: rgba(0, 0, 0, 0.25);
+  padding: 0.25em 0.25em;
+}
+
+.underlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 100em;
+}
+
+.underlay img {
+  background-position: cover;
+}
+</style>
