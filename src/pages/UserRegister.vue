52d87e70df71a0b93615d7e517be4bc62a928efb
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-13 10:46:23 +0100

    complete Introduction to VeeValidate

--- src/pages/UserRegister.vue
+++ src/pages/UserRegister.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="flex-grid justify-center">
     <div class="col-2">
-      <form @submit.prevent="register" class="card card-form">
+      <vee-form
+        :validation-schema="registerSchema"
+        @submit="register"
+        @invalid-submit="handleErrors"
+        class="card card-form"
+      >
         <h1 class="text-center">Register</h1>
         <div class="text-center push-top">
           <button @click="loginWithGoogle" class="btn-red btn-xsmall">
@@ -11,37 +16,54 @@
 
         <div class="form-group push-top">
           <label for="name">Full Name</label>
-          <input v-model="form.name" id="name" type="text" class="form-input" />
+          <vee-field
+            name="name"
+            v-model="name"
+            v-bind="nameAttrs"
+            id="name"
+            type="text"
+            class="form-input"
+          />
+          <vee-error-message class="error-message" name="name" />
         </div>
 
         <div class="form-group">
           <label for="username">Username</label>
-          <input
-            v-model="form.username"
+          <vee-field
+            name="username"
+            v-model="username"
+            v-bind="usernameAttrs"
             id="username"
             type="text"
             class="form-input"
           />
+          <vee-error-message class="error-message" name="username" />
         </div>
 
         <div class="form-group">
           <label for="email">Email</label>
-          <input
-            v-model="form.email"
+          <vee-field
+            name="email"
+            v-model="email"
+            v-bind="emailAttrs"
             id="email"
             type="email"
             class="form-input"
           />
+          <vee-error-message class="error-message" name="email" />
         </div>
 
         <div class="form-group">
           <label for="password">Password</label>
-          <input
-            v-model="form.password"
+          <vee-field
+            name="password"
+            v-model="password"
+            v-bind="passwordAttrs"
             id="password"
             type="password"
             class="form-input"
           />
+          <vee-error-message class="error-message" name="password" />
         </div>
 
         <div class="form-group">
@@ -51,13 +73,17 @@
               <img :src="avatarPreview" class="avatar-xlarge" />
             </div>
           </label>
-          <input
+          <vee-field
+            name="avatar"
             v-show="!avatarPreview"
+            v-model="avatar"
+            v-bind="avatarAttrs"
             id="avatar"
             type="file"
             @change="handleFileUpload"
             class="form-input"
           />
+          <vee-error-message class="error-message" name="avatar" />
         </div>
 
         <div class="form-actions">
@@ -69,7 +95,7 @@
             Already have an account?
           </router-link>
         </div>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -77,8 +103,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  ErrorMessage as VeeErrorMessage,
+  useForm,
+  GenericObject,
+  InvalidSubmissionContext,
+} from 'vee-validate';
+import { object, string } from 'zod';
+import { toTypedSchema } from '@vee-validate/zod';
+
 import type User from '@/types/User';
-import type UserRegisterRequest from '@/types/UserRegisterRequest';
+// import type UserRegisterRequest from '@/types/UserRegisterRequest';
 import type { FirebaseError } from 'firebase/app';
 import type FileUploadEvent from '@/types/FileUploadEvent';
 import { RouteName } from '@/enums/RouteName';
@@ -87,15 +124,28 @@ import useAppendRouteHelper from '@/helpers/appendRouteHelper';
 import objectHelper from '@/helpers/objectHelper';
 
 const { toSuccessRedirect } = useAppendRouteHelper();
+
 const error = ref('');
-const form = ref<UserRegisterRequest>({
-  name: '',
-  username: '',
-  email: '',
-  password: '',
-  avatarFile: null,
-  avatar: '',
+const avatarFile = ref<File | null>(null);
+
+const registerSchema = toTypedSchema(
+  object({
+    email: string().min(1, 'required'),
+    password: string().min(8, 'required'),
+    name: string().min(1, 'required'),
+    username: string().min(1, 'required'),
+    avatar: string().optional(),
+  })
+);
+const { values: form, defineField } = useForm({
+  validationSchema: registerSchema,
 });
+
+const [email, emailAttrs] = defineField('email');
+const [password, passwordAttrs] = defineField('password');
+const [name, nameAttrs] = defineField('name');
+const [username, usernameAttrs] = defineField('username');
+const [avatar, avatarAttrs] = defineField('avatar');
 
 const avatarPreview = ref<string | null>(null);
 
@@ -105,7 +155,7 @@ const router = useRouter();
 const handleFileUpload = (uploadEvent: Event) => {
   //console.log('UserRegister>handleFileUpload', uploadEvent);
 
-  form.value.avatarFile = (uploadEvent as FileUploadEvent).target!.files[0];
+  avatarFile.value = (uploadEvent as FileUploadEvent).target!.files[0];
   //console.log('UserRegister>handleFileUpload', form.value.avatarFile);
   const reader = new FileReader();
   //TODO: preview is not displayed
@@ -113,12 +163,30 @@ const handleFileUpload = (uploadEvent: Event) => {
     //console.log('reader.onload>result', readerEvent.target!.result as string);
     avatarPreview.value = readerEvent.target!.result as string;
   };
-  if (form.value.avatar) reader.readAsDataURL(form.value.avatarFile as Blob);
+  if (form.avatar) reader.readAsDataURL(avatarFile.value as Blob);
 };
-const register = async () => {
+
+const handleErrors = ({
+  errors,
+  results,
+}: InvalidSubmissionContext<GenericObject>) => {
+  // console.log('UserRegister>handleErrors>context', context);
+  console.log('UserRegister>handleErrors>errors', errors);
+  console.log('UserRegister>handleErrors>results', results);
+};
+
+const register = async (values: Record<string, unknown>) => {
+  if (values) {
+    console.log('UserRegister>register', values);
+  }
   //console.log('The form data >', form.value);
   const user = await useUserStore().registerUserWithEmailAndPassword({
-    ...form.value,
+    avatar: form.avatar!,
+    avatarFile: avatarFile.value,
+    email: form.email!,
+    name: form.name!,
+    username: form.username!,
+    password: form.password!,
   });
   if (!objectHelper().instanceOf<User>(user, 'username')) {
     error.value = (user as FirebaseError).message;
