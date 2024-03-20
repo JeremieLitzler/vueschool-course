63328a3b64a986f13dccf0504cb67877b7f40bb5
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-20 16:46:39 +0100

    complete NuxtLink for Navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <script lang="ts" setup></script>
 
 <template>
+  <section>
+    <p>anchor elements</p>
+    <nav><a href="/">Home</a> | <a href="/movies">Movies</a></nav>
+  </section>
+  <section>
+    <p>NuxLink</p>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link> |
+      <nuxt-link to="/movies">Movies</nuxt-link> |
+      <nuxt-link to="https://vueschool.io/courses" target="_blank"
+        >VueSchool</nuxt-link
+      >
+    </nav>
+  </section>
   <h1>Page: Movies</h1>
 </template>
 
