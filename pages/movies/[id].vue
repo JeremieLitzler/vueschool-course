e9319cb663fcfb9ad32484bdc28cf53456dad1a8
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-20 16:59:54 +0100

    complete Dynamic Route Params in Nuxt.js

---
+++ pages/movies/[id].vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+</script>
+
+<template>
+  <h1>Page: movies/{{ route.params.id }}</h1>
+</template>
+
+<style scoped></style>
--- pages/teams/[team-slug]/users/[userId].vue
+++ pages/teams/[team-slug]/users/[userId].vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <h1>Page: Movies</h1>
+  <div>
+    Page: teams/[team-slug]/users/[id]
+  </div>
 </template>
 
 <style scoped></style>
