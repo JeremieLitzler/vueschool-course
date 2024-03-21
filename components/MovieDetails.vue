cc3e066e242ebc719fb71dec95a9be3a7b2673b1
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-21 11:05:20 +0100

    complete Configure useAsyncData with Options

--- components/MovieDetails.vue
+++ components/MovieDetails.vue
@@ -2,18 +2,47 @@
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import type Movie from '@@/types/Movie';
+const nuxtApp = useNuxtApp();
 const route = useRoute();
 const init = ref(true);
-const ready = ref(false);
-ready.value = false;
-const { data } = await useAsyncData(() =>
-  $fetch(`${import.meta.env.VITE_OMDBAPI_URL}&i=${route.params.id}`)
+const { pending, data: movie } = await useLazyAsyncData<Movie>(
+  `/movies/${route.params.id}`,
+  (): Promise<Movie> => {
+    return $fetch(`${import.meta.env.VITE_OMDBAPI_URL}&i=${route.params.id}`);
+  },
+  {
+    default: () => null,
+    pick: ['Title', 'Plot', 'Poster', 'imdbID'],
+    /**
+     * Below is the same as above but quicker to write.
+     * Also, you get intellisense if you typed the Promise
+     * returned on the handler.
+     */
+    // transform(data) {
+    //   return {
+    //     Title: data.Title,
+    //     Plot: data.Plot,
+    //     Poster: data.Poster,
+    //     imdbID: data.imdbID,
+    //   };
+    // },
+    getCachedData(key) {
+      const data = nuxtApp.static.data[key] || nuxtApp.payload.data[key];
+      console.log('getCachedData>data', data);
+
+      if (!data || data === undefined) {
+        return;
+      }
+      return data;
+    },
+  }
 );
-const movie = ref<Movie | null>(data as unknown as Movie);
-ready.value = true;
+console.log('MovieDetails>data', movie.value);
+
+// const movie = ref<Movie | null>(data.value);
 </script>
 <template>
-  <section v-if="!ready && !init">Searching...</section>
+  <section v-if="pending">Fetching movie details...</section>
   <section v-else>
     <h1>{{ movie?.Title }}</h1>
 
@@ -21,6 +50,7 @@ ready.value = true;
     <p>
       {{ movie?.Plot }}
     </p>
+    <pre>{{ movie }}</pre>
   </section>
 </template>
 
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <MovieSearch />
+  <MovieSearchV2 />
 </template>
