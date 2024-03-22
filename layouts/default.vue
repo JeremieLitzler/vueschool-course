c2904b3e6811c7532a9239820181272a68b3d2de
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-22 09:18:02 +0100

    complete Abort Navigation or Redirect In Nuxt Middleware

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <nav>
     <nuxt-link to="/">Home</nuxt-link> |
     <nuxt-link to="/movies">Movies</nuxt-link> |
+    <nuxt-link to="/admin">Admin</nuxt-link> |
     <nuxt-link to="https://vueschool.io/courses" target="_blank"
       >VueSchool</nuxt-link
     >
---
+++ pages/admin.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <h1>Page: admin</h1>
+  <p>Hello user</p>
+</template>
+
+<style scoped></style>
